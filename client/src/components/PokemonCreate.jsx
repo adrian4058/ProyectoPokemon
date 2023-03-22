d8343af71d286d08dec425c0fb7e772ea1c9b045
adrian4058 <adrian_2016_@outlook.es>
2023-03-22 18:33:57 -0300

    rework form

--- client/src/components/PokemonCreate.jsx
+++ client/src/components/PokemonCreate.jsx
@@ -35,7 +35,10 @@ const validpokemon = (pokemon) => {
   if (!pokemon.types.length) {
     errors.types = "Se requiere al menos un tipo.";
   }
-  return errors;
+  for (const key in errors) {
+    return { [key]: errors[key] };
+  }
+  return {};
 };
 
 export default function PokemonCreate() {
@@ -43,6 +46,7 @@ export default function PokemonCreate() {
   const types = useSelector((state) => state.types);
 
   const [errors, setErrors] = useState({});
+  const [selectedCount, setSelectedCount] = useState(0);
 
   const [pokemon, setPokemon] = useState({
     name: "",
@@ -57,10 +61,25 @@ export default function PokemonCreate() {
   });
 
   const handleSelect = (e) => {
-    setPokemon({
-      ...pokemon,
-      types: [...pokemon.types, e.target.value],
-    });
+    const selectedType = e.target.value;
+
+    // Actualiza el estado de Pokemon con el nuevo tipo seleccionado
+    setPokemon((prevPokemon) => ({
+      ...prevPokemon,
+      types: [...prevPokemon.types, selectedType],
+    }));
+
+    // Incrementa el contador de selección
+    setSelectedCount((prevCount) => prevCount + 1);
+
+    // Deshabilita el select si se han seleccionado dos opciones
+    if (selectedCount >= 1 && selectedCount + 1 >= 2) {
+      const options = Array.from(e.target.options);
+      const unselectedOptions = options.filter((option) => !option.selected);
+      unselectedOptions.forEach((option) => {
+        option.disabled = true;
+      });
+    }
   };
 
   const inputChange = (e) => {
@@ -69,18 +88,20 @@ export default function PokemonCreate() {
       ...pokemon,
       [e.target.name]: e.target.value,
     });
-    setErrors(
-      validpokemon({
+    setErrors((prevState) => {
+      const newErrors = validpokemon({
         ...pokemon,
         [e.target.name]: e.target.value,
-      })
-    );
+      });
+      const firstKey = Object.keys(newErrors)[0];
+      return firstKey ? { [firstKey]: newErrors[firstKey] } : {};
+    });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
     dispatch(createPokemon(pokemon));
-    alert("Personaje creado con exito");
+    // alert("Personaje creado con exito");
     setPokemon({
       name: "",
       types: [],
@@ -99,118 +120,133 @@ export default function PokemonCreate() {
   }, []);
 
   return (
-    <div>
+    <div className="form">
       <NavBar currentComponent="pokemonCreate" />
-      <h3 className="title">Crear Pokemon</h3>
-      <form className="form" onSubmit={onSubmit}>
-        <div>
-          <label>Nombre: </label>
-          <input
-            onChange={inputChange}
-            type="text"
-            name="name"
-            value={pokemon.name}
-            className="input"
-          />{" "}
-          {errors.name && <p className="error"> {errors.name}</p>}
+      <div className="form-content">
+        <div className="form-title">
+          <h3 className="title">Crea tu Pokémon</h3>
+          <form className="form-submit" onSubmit={onSubmit}>
+            <div className="form-total">
+              <div className="children-one margin">
+                <div>
+                  <label>Nombre: </label>
+                  <input
+                    onChange={inputChange}
+                    type="text"
+                    name="name"
+                    value={pokemon.name}
+                    className="input"
+                  />{" "}
+                  {errors.name && <p className="error"> {errors.name}</p>}
+                </div>
+                <div>
+                  <label htmlFor="">Imagen: </label>
+                  <input
+                    onChange={inputChange}
+                    name="image"
+                    type="text"
+                    value={pokemon.image}
+                    className="input"
+                  />{" "}
+                  {errors.image && <p className="error"> {errors.image}</p>}
+                </div>
+                <div>
+                  <label htmlFor="">Vida: </label>
+                  <input
+                    onChange={inputChange}
+                    name="hitpoint"
+                    type="number"
+                    value={pokemon.hitpoint}
+                    className="input"
+                  />{" "}
+                  {errors.hitpoint && (
+                    <p className="error"> {errors.hitpoint}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="">Fuerza: </label>
+                  <input
+                    onChange={inputChange}
+                    name="attack"
+                    type="number"
+                    value={pokemon.attack}
+                    className="input"
+                  />{" "}
+                  {errors.attack && <p className="error"> {errors.attack}</p>}
+                </div>
+              </div>
+              <div className="children-two margin">
+                <div>
+                  <label htmlFor="">Defensa: </label>
+                  <input
+                    onChange={inputChange}
+                    name="defense"
+                    type="number"
+                    value={pokemon.defense}
+                    className="input"
+                  />{" "}
+                  {errors.defense && <p className="error"> {errors.defense}</p>}
+                </div>
+                <div>
+                  <label htmlFor="">Velocidad: </label>
+                  <input
+                    onChange={inputChange}
+                    name="speed"
+                    type="number"
+                    value={pokemon.speed}
+                    className="input"
+                  />{" "}
+                  {errors.speed && <p className="error"> {errors.speed}</p>}
+                </div>
+                <div>
+                  <label htmlFor="">Altura: </label>
+                  <input
+                    onChange={inputChange}
+                    name="height"
+                    type="number"
+                    value={pokemon.height}
+                    className="input"
+                  />{" "}
+                  {errors.height && <p className="error"> {errors.height}</p>}
+                </div>
+                <div>
+                  <label htmlFor="">Peso: </label>
+                  <input
+                    onChange={inputChange}
+                    name="weight"
+                    type="number"
+                    value={pokemon.weight}
+                    className="input"
+                  />{" "}
+                  {errors.weight && <p className="error"> {errors.weight}</p>}
+                </div>
+              </div>
+              <p className="types-s margin">
+                <select onChange={handleSelect}>
+                  {types.map((e) => (
+                    <option value={e.name}>{e.name}</option>
+                  ))}
+                </select>
+                <ul>
+                  <li>{pokemon.types.map((e) => e + " , ")}</li>
+                </ul>
+              </p>{" "}
+              {errors.types && <p className="error"> {errors.types}</p>}
+              <div className="buttons argin">
+                <Link to="/home">
+                  <button type="submit" className="atras">
+                    Atrás
+                  </button>
+                </Link>
+                <button type="submit" className="bottom">
+                  Crear Pokemon
+                </button>
+                <a href="/create">Limpiar Form</a>
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="">Imagen: </label>
-          <input
-            onChange={inputChange}
-            name="image"
-            type="text"
-            value={pokemon.image}
-            className="input"
-          />{" "}
-          {errors.image && <p className="error"> {errors.image}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Vida: </label>
-          <input
-            onChange={inputChange}
-            name="hitpoint"
-            type="number"
-            value={pokemon.hitpoint}
-            className="input"
-          />{" "}
-          {errors.hitpoint && <p className="error"> {errors.hitpoint}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Fuerza: </label>
-          <input
-            onChange={inputChange}
-            name="attack"
-            type="number"
-            value={pokemon.attack}
-            className="input"
-          />{" "}
-          {errors.attack && <p className="error"> {errors.attack}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Defensa: </label>
-          <input
-            onChange={inputChange}
-            name="defense"
-            type="number"
-            value={pokemon.defense}
-            className="input"
-          />{" "}
-          {errors.defense && <p className="error"> {errors.defense}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Velocidad: </label>
-          <input
-            onChange={inputChange}
-            name="speed"
-            type="number"
-            value={pokemon.speed}
-            className="input"
-          />{" "}
-          {errors.speed && <p className="error"> {errors.speed}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Altura: </label>
-          <input
-            onChange={inputChange}
-            name="height"
-            type="number"
-            value={pokemon.height}
-            className="input"
-          />{" "}
-          {errors.height && <p className="error"> {errors.height}</p>}
-        </div>
-        <div>
-          <label htmlFor="">Peso: </label>
-          <input
-            onChange={inputChange}
-            name="weight"
-            type="number"
-            value={pokemon.weight}
-            className="input"
-          />{" "}
-          {errors.weight && <p className="error"> {errors.weight}</p>}
-        </div>
-        <p className="types-s">
-          <select onChange={handleSelect}>
-            {types.map((e) => (
-              <option value={e.name}>{e.name}</option>
-            ))}
-          </select>
-          <ul>
-            <li>{pokemon.types.map((e) => e + " , ")}</li>
-          </ul>
-        </p>{" "}
-        {errors.types && <p className="error"> {errors.types}</p>}
-        <Link to="/home">
-          <button type="submit" className="atras">
-            Atrás
-          </button>
-        </Link>
-        <button type="submit" className="bottom">
-          Crear Pokemon
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
