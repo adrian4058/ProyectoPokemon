a475904d2c781e6dd3af1456c9683c2cdc61f4cf
adrian4058 <adrian_2016_@outlook.es>
2023-03-29 00:47:19 -0300

    deploy backend

--- api/src/db.js
+++ api/src/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOSTPG, DB_PORT, DB_DATABASE } = process.env;
@@ -9,7 +10,7 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    // dialectModule: pg,
+    dialectModule: pg,
   }
 );
 const basename = path.basename(__filename);
